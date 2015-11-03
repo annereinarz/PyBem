03e9232471f55069b9185531fb1a7a09e066e741
Anne Reinarz <cardanomnom@users.noreply.github.com>
2015-11-03 10:05:02 +0000

    delete start anew

--- BEM/memoize.py
+++
@@ -1,8 +0,0 @@
-def memoize(f):
-    """ Memoization decorator for a function taking a single argument. For multiple arguments, call the function with a tuple. """
-    class memodict(dict):
-        def __missing__(self, key):
-            ret = self[key] = f(key)
-            return ret 
-    return memodict().__getitem__
-
--- PanelClustering/covering.py
+++
@@ -1,6 +0,0 @@
-from clustering import panel
-
-def minimalCovering(admissable, cluster):
-    if cluster.isPanel() or admissable(cluster):
-        return [cluster]
-    return sum([ minimalCovering(admissable, c) for c in cluster.children ], [])
